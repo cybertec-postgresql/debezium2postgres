629d79da94b1350fedadd9c3f4dc41b7732c3ebf
Pavlo Golub <pavlo.golub@gmail.com>
2020-12-08 16:18:59 +0100

    increase kafka module test coverage #11 #CMN-116

--- internal/kafka/kafka_test.go
+++ internal/kafka/kafka_test.go
@@ -50,14 +50,17 @@ type mockKafkaReader struct {
 }
 
 func (r *mockKafkaReader) ReadMessage(ctx context.Context) (kafka.Message, error) {
-	if r.ReadMessageHandler != nil {
-		return r.ReadMessageHandler()
-	}
 	if ctx.Err() != nil {
 		return kafka.Message{}, ctx.Err()
 	}
+	if r.ReadMessageHandler != nil {
+		return r.ReadMessageHandler()
+	}
 	time.Sleep(500 * time.Millisecond)
-	return kafka.Message{}, nil
+	return kafka.Message{
+		Value: []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"},{"type":"string","optional":false,"field":"first_name"},{"type":"string","optional":false,"field":"last_name"},{"type":"string","optional":false,"field":"email"},{"type":"int64","optional":true,"field":"__source_ts_ms"},{"type":"string","optional":true,"field":"__db"},{"type":"string","optional":true,"field":"__table"},{"type":"string","optional":true,"field":"__op"},{"type":"string","optional":true,"field":"__deleted"}],"optional":false,"name":"dbserver1.inventory.customers.Value"},"payload":{"id":1003,"first_name":"Edward","last_name":"Walker","email":"ed@walker.com","__source_ts_ms":0,"__db":"inventory","__table":"customers","__op":"c","__deleted":"false"}}`),
+		Key:   []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"dbserver1.inventory.customers.Key"},"payload":{"id":1003}}`),
+	}, nil
 }
 
 func (r *mockKafkaReader) Close() error {
@@ -68,14 +71,19 @@ func TestConsumeTopic(t *testing.T) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
+	getReader = func(brokers []string, topic string) kafkaReader {
+		return &mockKafkaReader{}
+	}
 	consumeTopic(ctx, []string{"foo", "bar"}, "baz", make(chan Message, 10))
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
 	getReader = func(brokers []string, topic string) kafkaReader {
-		return &mockKafkaReader{}
+		return &mockKafkaReader{
+			ReadMessageHandler: func() (kafka.Message, error) {
+				time.Sleep(500 * time.Millisecond)
+				return kafka.Message{}, nil
+			}}
 	}
 	consumeTopic(ctx, []string{"foo", "bar"}, "baz", make(chan Message, 10))
 }
---
+++ internal/kafka/message_test.go
@@ -0,0 +1,38 @@
+package kafka
+
+import (
+	"testing"
+
+	kafka "github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMessage(t *testing.T) {
+	m := kafka.Message{
+		Value: []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"},{"type":"string","optional":false,"field":"first_name"},{"type":"string","optional":false,"field":"last_name"},{"type":"string","optional":false,"field":"email"},{"type":"int64","optional":true,"field":"__source_ts_ms"},{"type":"string","optional":true,"field":"__db"},{"type":"string","optional":true,"field":"__table"},{"type":"string","optional":true,"field":"__op"},{"type":"string","optional":true,"field":"__deleted"}],"optional":false,"name":"dbserver1.inventory.customers.Value"},"payload":{"id":1003,"first_name":"Edward","last_name":"Walker","email":"ed@walker.com","__source_ts_ms":0,"__db":"inventory","__table":"customers","__op":"c","__deleted":"false"}}`),
+		Key:   []byte(`{"schema":{"type":"struct","fields":[{"type":"int32","optional":false,"field":"id"}],"optional":false,"name":"dbserver1.inventory.customers.Key"},"payload":{"id":1003}}`),
+	}
+	msg, err := NewMessage(m)
+	assert.NoError(t, err)
+	assert.NotNil(t, msg)
+
+	m.Value = []byte(`{"schema":null, "payload":null}`)
+	msg, err = NewMessage(m)
+	assert.Error(t, err, "Corrupted value payload")
+	assert.Nil(t, msg)
+
+	m.Value = []byte{}
+	msg, err = NewMessage(m)
+	assert.Error(t, err, "Corrupted value")
+	assert.Nil(t, msg)
+
+	m.Key = []byte(`{"schema":null, "payload":null}`)
+	msg, err = NewMessage(m)
+	assert.Error(t, err, "Corrupted key payload")
+	assert.Nil(t, msg)
+
+	m.Key = []byte{}
+	msg, err = NewMessage(m)
+	assert.Error(t, err, "Corrupted key")
+	assert.Nil(t, msg)
+}
